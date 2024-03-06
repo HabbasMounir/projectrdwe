9e314ed843945448c905a1f6f8714754273ce626
mounir habbas <Mounirawt@gmail.com>
2024-03-06 23:48:56 +0100

    last

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,14 +11,14 @@ import Logo from './../../components/logo/logo';
 
 export default function Home() {
 
-  const [slideper, setSlideper] = useState(0)
+  const [slideperone, setslideperone] = useState(0)
   const sliderRef = useRef(null);
 
   const handleprev = () => {
-    if (slideper == -50) setSlideper(prev => prev + 50)
+    if (slideperone == -50) setslideperone(prev => prev + 50)
   };
   const handlenext = () => {
-    if (slideper == 0) setSlideper(prev => prev - 50)
+    if (slideperone == 0) setslideperone(prev => prev - 50)
   };
   return (
     <>
@@ -57,7 +57,7 @@ export default function Home() {
             <div className={Style.handle}>
               <span onClick={handleprev}
                 style={{
-                  opacity: slideper == 0 ? .5 : 1
+                  opacity: slideperone == 0 ? .5 : 1
                 }}
               >
                 <svg
@@ -72,13 +72,18 @@ export default function Home() {
                     d="M7.41 1.41L2.83 6l4.58 4.59L6 12 0 6l6-6 1.41 1.41z"
                   ></path>
                 </svg>
-                Previous
+                <span className={Style.fre}>
+                Previous 
+                </span>
+
               </span>
               <span onClick={handlenext}
                 style={{
-                  opacity: slideper == -50 ? .5 : 1
+                  opacity: slideperone == -50 ? .5 : 1
                 }}
-              >Next
+              >                <span className={Style.fre}>
+                Next
+                </span >
 
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -99,113 +104,35 @@ export default function Home() {
           <div className={Style.SlideRate} ref={sliderRef}>
             <div className={Style.slider}
               style={{
-                translate: `${slideper}%`
+                translate: `${slideperone}%`
               }}
               ref={sliderRef}>
-              <div className={Style.slideone}>
+              {/* <div className={Style.slideone}> */}
                 <RateCard rate="2" message={'i want to saygk, sh'} name={'Mounir'}></RateCard>
                 <RateCard rate="5" message={'بيبسيسبك شبسيك كبشككبشككبشككبشككبشككبشككبشككبشككبشككبشككببشككبشككبشك كشبسب '} name={'Mounir'}></RateCard>
                 <RateCard rate="1" message={'i want to sjghay sh'} name={'Mounir'}></RateCard>
-              </div>
-              <div className={Style.slidetwo}>
+              {/* </div> */}
+              {/* <div className={Style.slidetwo}> */}
                 <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
                 <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
                 <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
-              </div>
+              {/* </div> */}
             </div>
           </div>
           <div className={Style.sliderBalls}> <span onClick={handleprev}
             style={{
-              opacity: slideper == -50 ? .1 : 1
+              opacity: slideperone == -50 ? .1 : 1
             }}
           ></span>
             <span onClick={handlenext}
               style={{
-                opacity: slideper == 0 ? .1 : 1
+                opacity: slideperone == 0 ? .1 : 1
               }}
             ></span>  </div>
 
           {/* <SlideRate></SlideRate> */}
         </section>
-        <section className={Style.rate}>
-          <div className={Style.rate__header}>
-            <div className={Style.header}>
-              <h1>Our Members Opinions </h1>
-              <h2>Don’t take our word for it. Trust our customers</h2>
-            </div>
-            <div className={Style.handle}>
-              <span onClick={handleprev}
-                style={{
-                  opacity: slideper == 0 ? .5 : 1
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="8"
-                  height="12"
-                  fill="none"
-                  viewBox="0 0 8 12"
-                >
-                  <path
-                    fill="#fff"
-                    d="M7.41 1.41L2.83 6l4.58 4.59L6 12 0 6l6-6 1.41 1.41z"
-                  ></path>
-                </svg>
-                Previous
-              </span>
-              <span onClick={handlenext}
-                style={{
-                  opacity: slideper == -50 ? .5 : 1
-                }}
-              >Next
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="8"
-                  height="12"
-                  fill="none"
-                  viewBox="0 0 8 12"
-                >
-                  <path
-                    fill="#fff"
-                    d="M.59 10.59L5.17 6 .59 1.41 2 0l6 6-6 6-1.41-1.41z"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-
-          </div>
-          <div className={Style.SlideRate} ref={sliderRef}>
-            <div className={Style.slider}
-              style={{
-                translate: `${slideper}%`
-              }}
-              ref={sliderRef}>
-              <div className={Style.slideone}>
-                <RateCard rate="2" message={'i want to saygk, sh'} name={'Mounir'}></RateCard>
-                <RateCard rate="5" message={'بيبسيسبك شبسيك كبشككبشككبشككبشككبشككبشككبشككبشككبشككبشككببشككبشككبشك كشبسب '} name={'Mounir'}></RateCard>
-                <RateCard rate="1" message={'i want to sjghay sh'} name={'Mounir'}></RateCard>
-              </div>
-              <div className={Style.slidetwo}>
-                <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
-                <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
-                <RateCard rate="1" message={'i want to say sh'} name={'Mounir'}></RateCard>
-              </div>
-            </div>
-          </div>
-          <div className={Style.sliderBalls}> <span onClick={handleprev}
-            style={{
-              opacity: slideper == -50 ? .1 : 1
-            }}
-          ></span>
-            <span onClick={handlenext}
-              style={{
-                opacity: slideper == 0 ? .1 : 1
-              }}
-            ></span>  </div>
-
-          {/* <SlideRate></SlideRate> */}
-        </section>
+    
 
         <AboutUsSection />
         <FooterSection />
